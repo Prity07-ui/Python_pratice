2d514d35b002f2413d80b1945af768d9c2eee5a4
Prity <pk3183121@gmail.com>
2026-04-27 17:07:36 +0530

    Add files via upload

---
+++ Input.py
@@ -0,0 +1,11 @@
+# Input in python :- used to accept value from user
+# name = input("Enter Your name: ")
+# print("your name is :" , name)
+
+# val = input("Enter some value: ")
+# print(type(val), val)
+
+# Type Cast
+int("13")
+val = float(input("Enter some value:"))
+print(type(val),val)
