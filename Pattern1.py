a6787b8caf5375c58a4ef5dc95e88fec8a78f7b6
Prity <pk3183121@gmail.com>
2026-05-01 10:53:05 +0530

    Add files via upload

---
+++ Pattern1.py
@@ -0,0 +1,19 @@
+# Pattern
+""" *
+    **
+    ***
+    ****
+    ***** """
+
+ for i in range(1,6):
+    print("*" * i)
+
+
+#Reverse pattern
+
+ for i in range(5, 0, -1):
+    print("*" * i)
+
+
+
+   
