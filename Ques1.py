d17533429d25098d848778a4a2b41f6b29fa4645
Prity <pk3183121@gmail.com>
2026-05-04 18:41:18 +0530

    Add files via upload

---
+++ Ques1.py
@@ -0,0 +1,6 @@
+# write a program to input 2 numbers & print their sum
+
+num1 = int (input("Enter first number:"))
+num2=int(input("Enter Second number :"))
+sum =num1 + num2
+print("The sum of", num1, amd num2, "is",sum)
