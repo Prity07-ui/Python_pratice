120c49aa59ed82189695cffa81f636937b594dc7
Prity <pk3183121@gmail.com>
2026-05-09 23:56:44 +0530

    Add files via upload

---
+++ List.py
@@ -0,0 +1,33 @@
+# List
+
+list=[1,2,3,4,5,6]
+print(list)
+
+# Append 
+list =[3,4,6,5,7,2,5,6,5]
+list.append(45)
+print(list)
+
+# count
+list =[3,4,6,5,7,2,5,6,5]
+list.count(5)
+print(list)
+
+#pop
+list =[3,4,6,5,7,2,5,6,5]
+list.pop(5)
+print(list)
+
+#Remove
+list =[3,4,6,5,7,2,5,6,5]
+list.remove(5)
+print(list)
+
+#index
+list =[3,4,6,5,7,2,5,6,5]
+list.index(5)
+print(list)
+
+
+
+
