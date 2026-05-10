909e49660b5acc3bd6d7d4493ce0e95884eea5ff
Prity <pk3183121@gmail.com>
2026-05-10 22:00:51 +0530

    Add files via upload

---
+++ Nested list.py
@@ -0,0 +1,34 @@
+# Nested list
+
+mertix =[[2,3],[5,6],[7,8],[9,10]]
+print(mertix)
+
+# Accessing elements
+mertix =[[2,3],[5,6],[7,8],[9,10]]
+print(mertix[0][1])
+
+# Empty list
+empty =[]
+print(empty)
+
+#Mixed  data types
+mixed = [1, "Hello", 3.14, [1,2,3]]
+print(mixed)
+
+#sort
+numbers = [5,2,9,6,10,14]
+numbers.sort()
+print(numbers)
+
+#revrese
+list1 = [1,2,3,4,5]
+print(list1[::-1])
+
+#length
+list2 = [1,2,3,4,5,6]
+print(len(list2))
+
+#insert
+list3 = [2,4,7,3,7,8]
+inserted = list3.insert(1,10)
+print(list3)
