4f70f53a1989bfda7d045008f28e3ba8524c040c
Prity <pk3183121@gmail.com>
2026-05-28 20:21:22 +0530

    Add files via upload

---
+++ Random variable.py
@@ -0,0 +1,13 @@
+#random variable
+
+import random
+
+print(random.randint(1, 100))
+
+#math
+
+import math
+
+print("Square Root:", math.sqrt(25))
+print("Power:", math.pow(2, 3))
+print("Pi Value:", math.pi)
