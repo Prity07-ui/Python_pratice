64b4be7233d8e684069e33066bfb19c86ef982d5
Prity <pk3183121@gmail.com>
2026-05-28 20:12:59 +0530

    Add files via upload

---
+++ Matplotlib.py
@@ -0,0 +1,13 @@
+#Matplotlib
+
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4]
+y = [10, 20, 25, 30]
+
+plt.plot(x, y)
+plt.title("Simple Graph")
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+
+plt.show()
